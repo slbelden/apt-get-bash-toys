214bafd8721eca841f3f4db4f8e52f5c6e55e979
slbelden <belden.stephen@pm.me>
2020-09-06 03:00:44 +0000

    add initial testing script

---
+++ apt-get-bash-toys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Uses apt to attempt to install slb's favorite toys that work on bash:
+# * cacafire (via caca-utils)
+# * cmatrix
+# * cowsay
+# * figlet
+# * fortune
+# * lolcat (via gem)
+#
+
+# i hope you have apt because i am not checking anyting yet
+sudo apt install caca-utils cmatrix cowsay figlet fortune
+
+# lolcat from Ubuntu apt too out of date, use ruby installer instead
+sudo gem install lolcat
+
