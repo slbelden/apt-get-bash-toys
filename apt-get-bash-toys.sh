211736d8b4cf1d161511c9de8b2091e6ec350193
Stephen L. Belden <belden.stephen@pm.me>
2020-09-05 21:14:03 -0600

    gotta install ruby before trying to use gem

--- apt-get-bash-toys.sh
+++ apt-get-bash-toys.sh
@@ -11,7 +11,7 @@
 
 # i hope you have apt because i am not checking anyting yet
 sudo apt update
-sudo apt install -y caca-utils cmatrix cowsay fortune sl
+sudo apt install -y caca-utils cmatrix cowsay fortune ruby sl
 
 # lolcat from Ubuntu apt too out of date, use ruby installer instead
 sudo gem install lolcat
