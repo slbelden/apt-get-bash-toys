97e0568d12e47e65ec686a634703b0f49ad39f36
Stephen L. Belden <belden.stephen@pm.me>
2020-09-05 21:10:10 -0600

    chage included utilities

--- apt-get-bash-toys.sh
+++ apt-get-bash-toys.sh
@@ -4,13 +4,13 @@
 # * cacafire (via caca-utils)
 # * cmatrix
 # * cowsay
-# * figlet
 # * fortune
 # * lolcat (via gem)
+# * sl
 #
 
 # i hope you have apt because i am not checking anyting yet
-sudo apt install caca-utils cmatrix cowsay figlet fortune
+sudo apt install -y caca-utils cmatrix cowsay fortune sl
 
 # lolcat from Ubuntu apt too out of date, use ruby installer instead
 sudo gem install lolcat
